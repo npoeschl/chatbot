fe79a23d2fc6ac5532e1de0f5da0d2881e0de93e
npoeschl <n.poeschl@gmx.de>
2024-05-20 13:26:50 +0100

    removed ptb version

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ from flask import Flask, request
 from datetime import datetime, time
 from dateutil.relativedelta import relativedelta
 import contract_dbqueries
-from telegram import __version__ as TG_VER
+#from telegram import __version__ as TG_VER
 
-try:
-    from telegram import __version_info__
-except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
-
-if __version_info__ < (20, 0, 0, "alpha", 1):
-    raise RuntimeError(
-        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
-        f"{TG_VER} version of this example, "
-        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
-    )
+#try:
+#    from telegram import __version_info__
+#except ImportError:
+#    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+#
+#if __version_info__ < (20, 0, 0, "alpha", 1):
+#   raise RuntimeError(
+#        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+#       f"{TG_VER} version of this example, "
+ #       f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+ #   )
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
     Application,
