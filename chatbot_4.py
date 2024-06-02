a30781947cdae4210bd9e74104418a41256e7f5e
npoeschl <n.poeschl@gmx.de>
2024-06-02 21:52:07 +0100

    yxyxc

--- chatbot_4.py
+++ chatbot_4.py
@@ -449,7 +449,9 @@ async def savecontract(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
     nextcanceldate = enddate - relativedelta(months=+int(context.user_data["noticeperiod"])) 
     context.user_data["nextcancellationdate"] = nextcanceldate
     context.user_data["userid"] = context._user_id
-    newContract = contract_dbqueries.saveContract(context.user_data[0])
+    newContract = contract_dbqueries.saveContract(context.user_data)
+    for x in newContract:
+        print(newContract[x])
     context.user_data["last_inserted_contract"] = newContract
 
     keyboard = []
